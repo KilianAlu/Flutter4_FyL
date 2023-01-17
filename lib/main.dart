1ca21997db1e1d1cdae82d0c804dd2c3a36ad6ea
KilianAlu <alu2019068@365.stucom.com>
2023-01-17 19:23:10 +0100

    crtl c crtl v

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+String email = "1";
+String contrasena = "3";
+String value = "0";
+int contador = 0;
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo2',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,14 +29,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FirstRoute(title: 'Login Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class FirstRoute extends StatefulWidget {
+  const FirstRoute({super.key, required this.title});
 
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => FirstRouteState();
+}
+
+class FirstRouteState extends State<FirstRoute> {
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -40,76 +52,129 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+              margin: const EdgeInsets.fromLTRB(100, 5, 100, 5),
+              child: Image.network(
+                "https://i0.wp.com/clay-atlas.com/wp-content/uploads/2021/02/Flutter_LOGO.png?resize=930%2C375&ssl=1",
+                fit: BoxFit.contain,
+                width: 120,
+              )),
+          Container(
+              margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+              child: SizedBox(
+                  width: 300,
+                  child: TextField(
+                      obscureText: false,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                      onSubmitted: (String value) {
+                        setState(() {
+                          email = value;
+                        });
+                      }))),
+          Container(
+              margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+              child: SizedBox(
+                  width: 300,
+                  child: TextField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Contraseña',
+                      ),
+                      onSubmitted: (String value) {
+                        setState(() {
+                          contrasena = value;
+                        });
+                      }))),
+          const Text("Forgot Password", style: TextStyle(color: Colors.blue)),
+          ElevatedButton(
+              child: const Text("Login"),
+              onPressed: () {
+                if (email == "Usuario" && contrasena == "12345") {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) => SecondRoute())));
+                }
+              })
+        ]),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SecondRoute extends StatefulWidget {
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
 
-  void _incrementCounter() {
+class _SecondRouteState extends State<SecondRoute> {
+  void suma() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      contador++;
+    });
+  }
+
+  void resta() {
+    setState(() {
+      contador--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Segunda pantalla'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          child: SizedBox(
+              child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+            child: Text(
+              "Bienvenido $email !",
+              style: const TextStyle(color: Colors.black, fontSize: 25),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(90, 0, 90, 0),
+            child: Image.network(
+              'https://assets.stickpng.com/images/585e4beacb11b227491c3399.png',
+              fit: BoxFit.contain,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          Text("$contador", style: const TextStyle(fontSize: 50)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                  heroTag: "suma",
+                  onPressed: suma, child: const Icon(Icons.add)),
+              FloatingActionButton(
+                  heroTag: "resta",
+                  onPressed: resta, child: const Icon(Icons.remove)),
+            ],
+          ),
+          ElevatedButton(
+              child: const Text('Volver'),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) =>  const MyApp()));
+              }),
+        ],
+      ))),
     );
   }
 }
